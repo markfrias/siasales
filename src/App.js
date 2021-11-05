2f4014c8aba02e9b6384a21311b89103647f1ac8
hansmacaraan <macaraanhans6@gmail.com>
2021-11-05 17:26:10 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Registration1 from './registration/registration1'
 import Registration2 from './registration/registration2'
 import Registration3 from './registration/registration3'
 import Registration4 from './registration/registration4'
+import SideMenu from './components/SideMenu';
+import CustomerManagement from './components/CustomerManagement';
 
 import {
   BrowserRouter as Router,
@@ -17,8 +19,8 @@ import Login from './login/login';
 
 function App() {
   return (
-    
-    <Router>
+    <div className="App">
+          <Router>
       <nav>
           <ul>
             <Link to="/registration1">
@@ -46,9 +48,20 @@ function App() {
           <Route path="/registration3" component={Registration3} />
           <Route path="/registration4" component={Registration4} />
           <Route path="/login" component={Login}/>
+          <Route path="/customers" component={CustomerManagement} />
         </Switch>
       
     </Router>
+
+    <div className="row gx-3">
+      <div className="col-lg-auto w-5 col-sm-0 col-0">
+          <SideMenu/>
+      </div>
+      <div className="col">
+        <CustomerManagement/>
+      </div>
+    </div>
+</div>
 
 
   );
