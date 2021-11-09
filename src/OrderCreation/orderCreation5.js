a452d05e7e87588bc3a9bc126af359232fe10058
Kenneth <kennfamarin@gmail.com>
2021-11-09 14:33:33 +0800

    updated sales creation order

--- src/OrderCreation/orderCreation5.js
+++ src/OrderCreation/orderCreation5.js
@@ -4,6 +4,7 @@ import 'semantic-ui-css/semantic.min.css'
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
 
 
 
@@ -41,48 +42,223 @@ function orderCreation5() {
         </div>
 
         <div class="cust-info5">
-          <h1>Customer Information</h1>
+            <div class="customer5">
+                <h1>Customer Information</h1>
+            </div>
+            <div class="billing5">
+                <h1>Billing Information</h1>
+            </div>
         </div>
 
         <div class="cust-details5">
-            <div class="details-left5">
-                <div class="left5">
-                    <h5>Customer ID:</h5>
-                    <p>2208752585</p>
-                </div>
+            <div class="info-left5">
+                <div class="details-left5">
+                    <div class="left5">
+                        <h5>Customer ID:</h5>
+                        <p>2208752585</p>
+                    </div>
                     
-                <div class="left5">
-                    <h5>Customer Name:</h5>
-                    <p>Toyota Motors Philippines</p>
+                    <div class="left5">
+                        <h5>Customer Name:</h5>
+                        <p>Toyota Motors Philippines</p>
+                    </div>
+
+                    <div class="left5">
+                        <h5>Address:</h5>
+                        <div class="address-info5">
+                            <p>123 Toyota Drive</p>
+                            <p>Silang Cavite 4118</p>
+                            <p>Philippines</p>
+                        </div>
+                    </div>
                 </div>
 
-                <div class="left5">
-                    <h5>Address:</h5>
-                    <div class="address-info5">
-                      <p>123 Toyota Drive</p>
-                      <p>Silang Cavite 4118</p>
-                      <p>Philippines</p>
+                <div class="details-right5">
+                    <div class="right5">
+                        <h5>Contact Number:</h5>
+                        <p>09273344806</p>
+                    </div>
+
+                    <div class="right5">
+                        <h5>Email Adress:</h5>
+                        <p>customer@toyota.ph</p>
+                    </div>
+
+                    <div class="right5">
+                        <h5>Point of Contact:</h5>
+                        <p>Demi Andres</p>
                     </div>
                 </div>
             </div>
 
-            <div class="details-right5">
-                <div class="right5">
-                    <h5>Contact Number:</h5>
-                    <p>09273344806</p>
+            <div class="info-right5">
+                <div class="details-left5">
+                    <div class="left5">
+                        <h5>Billing Address:</h5>
+                        <div class="address-info5">
+                            <p>123 Toyota Drive</p>
+                            <p>Silang Cavite 4118</p>
+                            <p>Philippines</p>
+                        </div>
+                    </div>
+
+                    <div class="right5">
+                        <h5>Contact Number:</h5>
+                        <p>09778524444</p>
+                    </div>
                 </div>
 
-                <div class="right5">
-                    <h5>Email Adress:</h5>
-                    <p>customer@toyota.ph</p>
-                </div>
+                <div class="details-right5">
+                    <div class="right5">
+                        <h5>Email Adress:</h5>
+                        <p>customer@toyota.ph</p>
+                    </div>
 
-                <div class="right5">
-                    <h5>Point of Contact:</h5>
-                    <p>Demi Andres</p>
+                    <div class="right5">
+                        <h5>Person/Department to Alert:</h5>
+                        <p>Ferby Reyes</p>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <div class="cust-info5">
+            <div class="customer5">
+                <h1>Shipping Information</h1>
+            </div>
+        </div>
+
+        <div class="cust-details5">
+            <div class="info-left5">
+                <div class="details-left5">
+                    <div class="left5">
+                        <h5>Address:</h5>
+                        <div class="address-info5">
+                            <p>123 Toyota Drive</p>
+                            <p>Silang Cavite 4118</p>
+                            <p>Philippines</p>
+                        </div>
+                    </div>
+
+                    <div class="left5">
+                        <h5>Contact Number:</h5>
+                        <p>09273344806</p>
+                    </div>
+
+                    <div class="left5">
+                        <h5>Email Adress:</h5>
+                        <p>customer@toyota.ph</p>
+                    </div>
+
+                    <div class="left5">
+                        <h5>Person/Department to Alert:</h5>
+                        <p>Ferby Reyes</p>
+                    </div>
+                </div>
+
+                <div class="details-right5">
+                    <div class="right5">
+                        <h5>Ship via:</h5>
+                        <p>FedEx</p>
+                    </div>
+                    
+                    <div class="right5">
+                        <h5>Shipping Method:</h5>
+                        <p>FedEx</p>
+                    </div>
+
+                    <div class="right5">
+                        <h5>Payment Choice:</h5>
+                        <p>Cash</p>
+                    </div>
+
+                    <div class="right5">
+                        <h5>Delivery Date:</h5>
+                        <p>January 15, 2022</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="cust-info5">
+            <div class="customer5">
+                <h1>Order Details</h1>
+            </div>
+        </div>
+
+        <div class="order5">
+            <div class="order-table5">
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Item ID:</th>
+                            <th>Item Name:</th>
+                            <th>Quantity:</th>
+                            <th>Total Price:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>0123456789</td>
+                            <td>Toyota Philippines</td>
+                            <td>2</td>
+                            <td>Php 150,000</td>
+                        </tr>
+                        <tr>
+                            <td>012345678</td>
+                            <td>Toyota Philippines</td>
+                            <td>55</td>
+                            <td>Php 150,000</td>
+                        </tr>
+                        <tr>
+                            <td>01234567</td>
+                            <td>Toyota Philippines</td>
+                            <td>777</td>
+                            <td>Php 0</td>
+                        </tr>
+                        <tr>
+                            <td>012345678</td>
+                            <td>Toyota Philippines</td>
+                            <td>251</td>
+                            <td>Php 50,000</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
+
+            <div class="order-number5">
+                <div class="cont-mid5">
+                    <h4>Subtotal:</h4>
+                    <h4>Discount:</h4>
+                    <h4>Subtotal Less Discount:</h4>
+                    <h4>Total Tax:</h4>
+                    <h4>Shipping/Handling Fee:</h4>
+                    <h4>Other:</h4>
+
+                <div class="total5">
+                    <h4>Total:</h4>
+                </div>
+                </div>
+
+                <div class="cont-right5">
+                    <h4>300,000</h4>
+                    <h4>10,000</h4>
+                    <h4>10,000</h4>
+                    <h4>10,000</h4>
+                    <h4>10,000</h4>
+                    <h4>10,000</h4>
+
+                <div class="total5">
+                    <h4>PhP 350,000</h4>
+                </div>
+                </div>
+            </div>
+
+        </div>
+
+            <div class="btn5">
+                <Button type="submit">Submit</Button>
+            </div>
 
     </div>  
 
