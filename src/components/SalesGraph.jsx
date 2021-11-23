37e09a07c259a734327b41b8758f6186e772c083
Mark Jabel Frias <friasmarkjabel@gmail.com>
2021-11-23 15:21:35 +0800

    Add table to sales analysis page

--- src/components/SalesGraph.jsx
+++ src/components/SalesGraph.jsx
@@ -43,11 +43,6 @@ const SalesGraph = () => {
             <div className="mt-3">
                 <h3 className="fw-bolder">Sales Analysis</h3>
                 <p>Last updated December 2, 2021 5:30pm</p>
-
-                <div className="d-flex mt-4">
-                    <Link className="btn btn-info btn-sm fw-bolder rounded-pill" to="/sales/graph">Graph</Link>
-                    <Link className="btn btn-sm fw-bolder rounded-pill ms-3" to="/sales">Table Data</Link>
-                </div>
             </div>
 
             <div className="mt-4">
@@ -70,6 +65,40 @@ const SalesGraph = () => {
                         <div className="col-sm-3">
                             <p>y-axis - average individual spending</p>
                         </div>
+                    </div>
+                </div>
+
+                <div className="mt-4 ms-5">
+                    <div className="table-responsive">
+                        <table className="table table-striped">
+                            <thead className="">
+                                <tr>
+                                    {
+                                        [
+                                            "Average Spending", "Jan 2021", "Feb 2021", "Mar 2021", "Apr 2021", "May 2021",
+                                            "June 2021", "Jul 2021", "Aug 2021", "Sep 2021", "Oct 2021", "Nov 2021", "Dec 2021"
+                                        ].map((val, index) => (
+                                            <th className="fw-bolder" key={index}>{val}</th>
+                                        ))
+                                    }
+                                </tr>
+                            </thead>
+                            <tbody>
+                               
+                                <tr>
+                                    <td className="fw-bold"></td>
+                                    {data.map((val, index) => (
+                                        <td key={index}>
+                                            {val.cost}
+                                        </td>
+                                    ))}
+                                </tr>
+                            
+                            </tbody>
+                            <tbody>
+
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
